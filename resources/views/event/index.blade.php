e4e584c73ad6fb0b5c1c60967b23da20b6fba76b
Bayu Prasetyo <52435800+BayuBP79@users.noreply.github.com>
2024-08-05 01:11:10 +0700

    Update index.blade.php

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 sm:mx-6 lg:px-8 space-y-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <livewire:event.table />
         </div>
     </div>
